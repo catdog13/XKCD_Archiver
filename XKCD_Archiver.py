ebb25bc667c374289efa61d1563875ad6f84bd88
Thomas Hernandez <thomasah12@ymail.com>
2016-06-16 22:49:25 -0500

    work around for #1608 more string changes

--- XKCD_Archiver.py
+++ XKCD_Archiver.py
@@ -4,6 +4,7 @@ import re
 from time import sleep
 import os
 pic_dir = r'C:\Users\Tom\Documents\Python_Projects\XKCD_Archiver\pics'
+file_types = ('.png', '.jpg', '.gif')
 
 
 def current_comic_number():
@@ -28,15 +29,21 @@ def downloader(link):
     else:
         comic = url.json()
         comic_url = comic['img']
+        comic_number = str(comic['num'])
         file_type = comic['img'][-4:]
         comic_title = re.sub('[^a-zA-Z0-9\n\.]', ' ', comic['title'])
-        output = '{}\\#{} {}'.format(pic_dir, str(comic['num']), comic_title + file_type)
-        wget.download(comic_url, output)
-        print('#' + str(comic['num']) + ' is done')
+        if comic_url.endswith(file_types):
+            output = '{}\\#{} {}{}'.format(pic_dir, comic_number, comic_title, file_type)
+            wget.download(comic_url, output)
+            print('#{} is done'.format(comic_number))
+        else:
+            print('comic #{} doest not have a downloadable url'.format(comic_number))
+            output = '{}\\#{} {}.png'.format(pic_dir, comic_number, comic_title)
+            wget.download('http://i.imgur.com/removed.png', output)
 
 
 if __name__ == '__main__':
     for count in range(last_downloaded(), current_comic_number()):
-        comic_number = last_downloaded() + 1
-        downloader('http://xkcd.com/' + str(comic_number) + '/info.0.json')
+        new_comic_number = str(last_downloaded() + 1)
+        downloader('http://xkcd.com/{}/info.0.json'.format(new_comic_number))
         sleep(1)
